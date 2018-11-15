e51316fd75508d308084d40b50de657b9a893e25
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-11-15 12:31:03 -0500

    Added sorting functionality to filtered data. Created a global variable that records the currently displayed data as a list of objects

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,33 @@
-// from data.js
-var tableData = data;
-// identify tbody as the d3 select for tbody tag
-var tbody = d3.select("tbody");
-
-// Select button tag for input:
-var enter = d3.select("#filter-btn");
-
-// ----- Filter according to input text:
-
-enter.on("click", function() {
+function createTable(dataTable) {
+	/*
+	This function will create the table according to the
+	data table input. If data from data.js is the input, 
+	this will output the table with no filters or sorting.
+	*/
 
 	// Remove all rows in current table
-	var prevRows = d3.selectAll("td").remove()
+	var prevRows = d3.selectAll("td").remove();
 
-	// prevent page refresh
- 	d3.event.preventDefault();
+	// assign global variable to data to be displayed
+	window.currentData = dataTable;
 
- 	var filteredData = tableData;
+	// identify tbody as the d3 select for tbody tag
+	var tbody = d3.select("tbody");	
+
+	// run a function to append table	
+	dataTable.forEach(function(sighting) {
+	// console.log(sighting)
+		var row = tbody.append("tr");
+		Object.entries(sighting).forEach(function([key,value]) {
+
+			var cell = row.append("td").attr("class", key);
+			// $(this).addClass(key);
+			cell.text(value);
+		});
+	});
+};
+
+function filterData(filteredData) {
 
 	// collect inputs entered
 	var inputDate = d3.select("#datetime").property("value");
@@ -34,35 +45,55 @@ enter.on("click", function() {
 	if (inputShape != "") {
 		var filteredData = filteredData.filter(sighting => sighting.shape === inputShape);
 	};
-// console.log(filteredData)
-	// run a function to append table
-	filteredData.forEach(function(sighting) {
-// console.log(sighting)
-		var row = tbody.append("tr");
-		Object.entries(sighting).forEach(function([key,value]) {
 
-			var cell = row.append("td").attr("class", key);
-			// $(this).addClass(key);
-			cell.text(value);
-		});
-	});
-
-});
-
-// Add sorting for column headers!
-
-function sortTable(dt) {
-	d3.selectAll(".state").sort(function(x,y) {
-		return d3.ascending(x, y);
-	});
+	return filteredData;
 }
 
-// Things to try: 
+function handleSubmit() {
 
-// display the table when the website is refreshed
-// create function for enter.on seperate from the event
+	// prevent page refresh
+ 	d3.event.preventDefault();
 
-// when sorting, recreate the entire table: 
-// when the button for sorting is clicked, filter the data table
-//  - however remember the previous input values so sorting does not make a whole new table
- 
+ 	// Define lastSort as empty. When a column is sorted, lastSort will change
+ 	window.lastSort = "";
+
+	// run create table based on output of filter data
+	createTable(filterData(tableData));
+};
+
+// Sort Data function: 
+function sortTable(key) {
+
+	if (lastSort != key) {
+		lastSort = key;
+		sortedData = currentData.slice().sort(function(a,b) {
+			var x = a[key]; var y = b[key];
+			return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+		});
+	}
+	else {
+		lastSort = "";
+		sortedData = currentData.slice().sort(function(a,b) {
+			var x = a[key]; var y = b[key];
+			return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+		});
+	};
+
+	createTable(sortedData);
+};
+
+// When you First open the page:
+// import data from data.js and create table
+var tableData = data;
+
+// Define lastSort as empty. When a column is sorted, lastSort will change
+window.lastSort = "";
+
+createTable(tableData);
+
+// Select button tag for input:
+var enter = d3.select("#filter-btn");
+
+// When button is clicked, run handleSubmit
+enter.on("click", handleSubmit);
+
